15a862123b8b683683ae4888c188bbcf81838f5f
celer7841 <celer7841@wrdsb.ca>
2020-10-02 12:52:14 +0000

    finished

--- Main.java
+++ Main.java
@@ -9,7 +9,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  
+  // pre loop code
+  System.out.println("Starting to count");
     
+// create a counter variable to keep tarck of what number we are on
+int count = 1;
+// use a loop to count to 5
+while(true){
+  // print the number we are on
+  System.out.println(count);
+ 
+  // add one to count/ accumulater
+  count = count + 1;
+  if (count == 5){
+    break;
   }
 }
+
+ System.out.println("Done counting!");
+
+
+
+ int count2 = 1;
+ do{
+   System.out.println(count2);
+   count2 = count2 + 1;
+ }while (count2 <= 5);
+
+  }
+}  
